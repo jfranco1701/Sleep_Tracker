b2aab24a5638bbd264de7178f30c408be91712e1
jfranco1701 <jfranco@unomaha.edu>
2016-04-08 23:41:32 -0500

    Navigation drawer

--- app/src/main/java/com/app/joe/sleeptracker/MainActivity.java
+++ app/src/main/java/com/app/joe/sleeptracker/MainActivity.java
@@ -1,17 +1,30 @@
 package com.app.joe.sleeptracker;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private String mActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,44 +33,128 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Button connect = (Button) findViewById(R.id.buttonConnect);
-        connect.setOnClickListener(new View.OnClickListener(){
+        mDrawerList = (ListView)findViewById(R.id.navList);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
+
+        addDrawerItems();
+        setupDrawer();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+ /*       @Override
+        public boolean onOptionsItemSelected2(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+
+            Intent intent;
+
+            switch(item.getItemId()){
+                case R.id.action_settings:
+                    intent = new Intent(this, SettingsActivity.class);
+                    this.startActivity(intent);
+                    break;
+                case R.id.action_view:
+                    intent = new Intent(this, ViewActivity.class);
+                    this.startActivity(intent);
+                    break;
+                case R.id.action_about:
+                    intent = new Intent(this, AboutActivity.class);
+                    this.startActivity(intent);
+                    break;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }*/
+
+    private void addDrawerItems() {
+        String[] osArray = { "View Tracking History", "Settings", "About" };
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view){
-                Intent intent = new Intent(MainActivity.this, ScannerActivity.class);
-                startActivity(intent);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent;
+
+                switch (position) {
+                    case 0:
+                        intent = new Intent(view.getContext(), ViewActivity.class);
+                        startActivityForResult(intent, 0);
+                        break;
+                    case 1:
+                        intent = new Intent(view.getContext(), SettingsActivity.class);
+                        startActivityForResult(intent, 0);
+                        break;
+                    case 2:
+                        intent = new Intent(view.getContext(), AboutActivity.class);
+                        startActivityForResult(intent, 0);
+                        break;
+                }
             }
         });
     }
 
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Navigation!");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+    }
+
     @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+/*    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
+    }*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-        Intent intent;
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
-        switch(item.getItemId()){
-            case R.id.action_settings:
-                intent = new Intent(this, SettingsActivity.class);
-                this.startActivity(intent);
-                break;
-            case R.id.action_view:
-                intent = new Intent(this, ViewActivity.class);
-                this.startActivity(intent);
-                break;
-            case R.id.action_about:
-                intent = new Intent(this, AboutActivity.class);
-                this.startActivity(intent);
-                break;
+        // Activate the navigation drawer toggle
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
