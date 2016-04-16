a88639a0eb3af9ab895f706053f6efe79917ab37
jfranco1701 <jfranco@unomaha.edu>
2016-04-15 22:15:05 -0500

    Navigation drawer

--- app/src/main/java/com/app/joe/sleeptracker/SettingsActivity.java
+++ app/src/main/java/com/app/joe/sleeptracker/SettingsActivity.java
@@ -1,13 +1,24 @@
 package com.app.joe.sleeptracker;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private SharedPreferences preferenceSettings;
+    private SharedPreferences.Editor preferenceEditor;
+
+    private static final int PREFERENCE_MODE_PRIVATE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,49 @@ public class SettingsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button btnSelectDevice = (Button)findViewById(R.id.btnSelectDevice);
+        btnSelectDevice.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v){
+                        Intent intent = new Intent(SettingsActivity.this, MWScanActivity.class);
+                        startActivityForResult(intent, 1);
+                    }
+                }
+        );
+
+        TextView textViewSelectedDevice = (TextView)findViewById(R.id.textViewSelectedDevice);
+        textViewSelectedDevice.setText("Selected Device: " + readMACAddress());
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1) {
+            if(resultCode == Activity.RESULT_OK){
+                String strMAC=data.getStringExtra("MAC");
+
+                writeMACAddress(strMAC);
+
+                TextView textViewSelectedDevice = (TextView)findViewById(R.id.textViewSelectedDevice);
+                textViewSelectedDevice.setText("Selected Device: " + readMACAddress());
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                //Write your code if there's no result
+            }
+        }
+    }
+
+    private void writeMACAddress(String macAddress){
+        preferenceSettings = getPreferences(PREFERENCE_MODE_PRIVATE);
+        preferenceEditor = preferenceSettings.edit();
+        preferenceEditor.putString("macAddress", macAddress);
+        preferenceEditor.commit();
+    }
+
+    private String readMACAddress(){
+        preferenceSettings = getPreferences(PREFERENCE_MODE_PRIVATE);
+        String strMACAddress = preferenceSettings.getString("macAddress", "No Device Selected");
+        return strMACAddress;
+    }
 }
